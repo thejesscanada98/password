415050f6adb051d66a9d05b12b956b230b00ae8f
thejesscanada <canada.jess98@gmail.com>
2022-12-12 16:10:18 -0600

    added password variables

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
+var getOption = function () {
+  var passwordLength = window.prompt ("How many password you want?"); 
+  var isValid = parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128;
+
+  while (!isValid){
+    passwordLength = window.prompt ('Please choose a number between 8 to 128')
+    isValid = parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128;
+    console.log (isValid); 
+    console.log (passwordLength)
+
+  }
+
+  var isUppercase = window.confirm ("Do you want uppercase?"); 
+  var isLowercase = window.confirm ("Do you want lowercase?"); 
+  var isNumnber = window.confirm ('Do you want number?');
+  var isSpecalCharacter = window.confirm('Do you want speical character?');
+
+    return {
+    length: parseInt(passwordLength), 
+    uppercase: isUppercase,
+    lowercase : isLowercase,
+    number : isNumnber,
+    specialCharacter : isSpecalCharacter,
+  }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
